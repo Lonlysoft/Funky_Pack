9c4cf4b8423698fc98035cf09766b9d69ecc5ab9
pilis <probechecker@gmail.com>
2025-08-24 19:49:07 +0300

    Update items.js

--- items.js
+++ items.js
@@ -78,13 +78,13 @@ const itemCategories = {
 //constant for assets
 const ITEMS = {
 	0: {name: "clearItem", description: "enableCredits", value: 0, type: "creditsScene"},
-	1: {ID: 1, name: "penny", description: "a singular monetary solution costs $0.01", value: 1, type: "centMoney", usage: "CollectAndUse", ColType: "use", w: Number.parseInt(TILE_SIZE/4), h: Number.parseInt(TILE_SIZE/4), p: Number.parseInt(TILE_SIZE/4)},
-	2: {ID: 3, name: "coin", description: "a centural monetary solution costs $1.00", value: 1, type: "money", usage: "CollectAndUse", ColType: "use" , w: Number.parseInt(TILE_SIZE/4), h: Number.parseInt(TILE_SIZE/4), p: Number.parseInt(TILE_SIZE/6)},
+	1: {ID: 1, name: "penny", description: "A singular monetary solution costs $0.01", value: 1, type: "centMoney", usage: "CollectAndUse", ColType: "use", w: Number.parseInt(TILE_SIZE/4), h: Number.parseInt(TILE_SIZE/4), p: Number.parseInt(TILE_SIZE/4)},
+	2: {ID: 3, name: "coin", description: "A centural monetary solution costs $1.00", value: 1, type: "money", usage: "CollectAndUse", ColType: "use" , w: Number.parseInt(TILE_SIZE/4), h: Number.parseInt(TILE_SIZE/4), p: Number.parseInt(TILE_SIZE/6)},
 	//fruits
-	2: {ID: 2, name:"apple", description: "freah as ever give us the best", value: 2, type: "food", usage: "useLater", ColType: "solidObject", w: Number.parseInt(TILE_SIZE/2),p: Number.parseInt(TILE_SIZE/6), h: Number.parseInt(TILE_SIZE/4)},
-	3: {ID: 3, name:"pear", description: "fresh, but more watery. hunger -10, hp +7", value: 8, type: "food", usage: "useLater", ColType: "solidObject",
+	2: {ID: 2, name:"apple", description: "Fresh and gives us the best", value: 2, type: "food", usage: "useLater", ColType: "solidObject", w: Number.parseInt(TILE_SIZE/2),p: Number.parseInt(TILE_SIZE/6), h: Number.parseInt(TILE_SIZE/4)},
+	3: {ID: 3, name:"pear", description: "Fresh, but more watery", hunger -10, hp +7", value: 8, type: "food", usage: "useLater", ColType: "solidObject",
 w: Number.parseInt(TILE_SIZE/2),p: Number.parseInt(TILE_SIZE/6), h: Number.parseInt(TILE_SIZE/4)},
-	4: {ID: 4, name:"block", description: "completely solid object. I think you can only carry this if you're Nukko", value: 2, type: "food", usage: "useLater", ColType: "solidObject", w: TILE_SIZE, h: TILE_SIZE, p: TILE_SIZE},
+	4: {ID: 4, name:"block", description: "Completely solid object. I think you can only carry this if you're Nukko", value: 2, type: "food", usage: "useLater", ColType: "solidObject", w: TILE_SIZE, h: TILE_SIZE, p: TILE_SIZE},
 	5: {ID: 5, name: ""},
 	
 	50: {
@@ -107,16 +107,16 @@ w: Number.parseInt(TILE_SIZE/2),p: Number.parseInt(TILE_SIZE/6), h: Number.parse
 	["orange", ""],
 	
 	//liquids
-	["bottled water", "clear and Cold. the best drink. period."],
-	["super brite", "efervescent and sweet, all that goes nice with a well prepared popcorn"],
-	["sparkling water", "effervescent and bold, literally water soda."],
-	["tea", "fancy and finest as always"],
-	["coffee", "lights your energy, smells unique and gurgly if your stomach's empty"],
-	["energetic", "boosts energy, soda coffee, but with other taste"],
-	["hot cocoa", "natural fudge for your tummy. good for the Cold days of holidays"],
-	["tonic", "for the wealthy ones. 5 stars with a taste of alcohol"],
-	["wine", "fancy for all people, don't exaggerate."],
-	["SECUENDARRY RUSTY BRASS", "made with real rust. ALLERGICS: may contain dark mold, extrats of metal, gluten and milk"]
+	["bottled water", "Clear and cold. The best drink. Period."],
+	["super brite", "Effervescent and sweet, all that goes nice with a well prepared popcorn"],
+	["sparkling water", "Effervescent and bold, literally water soda."],
+	["tea", "Fancy and the finest as always"],
+	["coffee", "Lights your energy, smells unique and it's gurgly if your stomach is empty"],
+	["energetic", "Boosts energy, soda coffee, but with other taste"],
+	["hot cocoa", "Natural fudge for your tummy. Good for the cold days of holidays"],
+	["tonic", "For the wealthy ones. 5 stars with a taste of alcohol"],
+	["wine", "Fancy for all people, but don't exaggerate."],
+	["SECUENDARRY RUSTY BRASS", "Made with real rust. ALLERGENS: May contain dark mold, metal extracts, gluten and milk"]
 	
 	//healthies
 	["salad", ""],
@@ -137,17 +137,17 @@ w: Number.parseInt(TILE_SIZE/2),p: Number.parseInt(TILE_SIZE/6), h: Number.parse
 	["cookie package", ""]
 	
 	//drugs 
-	["coke", "maniac special: use it to get faster for a short period of time (HP gets melted)"],
-	["ozempic", "lose weight."],
-	["weed", "slowdown or fasten up. just a matter of choice."],
+	["coke", "Maniac special: use it to get faster for a short period of time (at the cost of HP getting melted)"],
+	["ozempic", "It helps losing weight."],
+	["weed", "Makes you faster or slower, it's just a matter of choice."],
 	
 	//non edibles. (are they?)
 	["remote control", ""],
-	["phone", "key item for communication. and meeting some vorny guys too"],
+	["phone", "The key item for communication, and to meet some vorny guys too"],
 	["laptop", ""],
 	["gramophone", ""],
 	["Dennis Folff disk", ""],
-	["??? toy", "not that he minds"],
+	["??? toy", "Not that he minds"],
 	["newspaper", ""],
 	
 	//tools 
@@ -179,4 +179,5 @@ w: Number.parseInt(TILE_SIZE/2),p: Number.parseInt(TILE_SIZE/6), h: Number.parse
 	[45499, "Ambulance", "", (TILE_SIZE*2)-10, (TILE_SIZE*2)-4, (TILE_SIZE*4)-10],
 	[4345445, "food Truck", "", "solidVehicle", (TILE_SIZE*2)-10, (TILE_SIZE*2)-4, (TILE_SIZE*6)-10]
 ]
+
 */
